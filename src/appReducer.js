45ac2bd5cc783cbb24f4381fdab2eab6cda43282
Shashank <112277097+OpenSourcePundit@users.noreply.github.com>
2023-11-26 05:01:57 +0000

    Update 10 files

--- src/appReducer.js
+++ src/appReducer.js
@@ -3,6 +3,34 @@ const initialState = {inventory:[],saleTransactions:[],loading:false,error:null}
 
 export const appReducer = (state = initialState,action) =>{
     switch(action.type){
+            case "APP_LOADING":
+            return{...state,loading:true}
+            case "ADD_INVENTORY_SUCCESS":
+            return{...state,loading:true}
+            case "ADD_INVENTORY_FAILURE":
+            return{...state,loading:true}
+            case "UPDATE_INVENTORY_SUCCESS":
+            return{...state,loading:true}
+            case "UPDATE_INVENTORY_FAILURE":
+            return{...state,loading:true}
+            case "ADD_SALE_SUCCESS":
+            return{...state,loading:true}
+            case "ADD_SALE_FAILURE":
+            return{...state,loading:true}
+            case "DELETE_INVENTORY_SUCCESS":
+            return{...state,loading:true}
+            case "DELETE_INVENTORY_FAILURE":
+            return{...state,loading:true}
+            case "FETCH_INVENTORY_SUCCESS":
+            return{...state,loading:true}
+            case "FETCH_INVENTORY_FAILURE":
+            return{...state,loading:true}
+            case "FETCH_SALES_SUCCESS":
+            return{...state,loading:true}
+            case "FETCH_SALES_FAILURE":
+            return{...state,loading:true}
+        default:
+            return(state)
         
 
 
